6d5d60022c3a62c6f7300d6618b6426e7ebeb661
Gregory Power <31050507+gregorywaynepower@users.noreply.github.com>
2026-03-17 21:33:29 -0400

    absolutely stuck

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 //! main.rs
 
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let port = "0";
+    let address = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await?;
+    Ok(())
 }
